85b7f7e015bb5f19aef905f5341281042223266b
Chris Powell <cpowell@prylis.com>
2013-08-24 15:23:16 -0700

    Initial Commit

---
+++ game_of_life/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  game_of_life
+//
+//  Created by Chris Powell on 8/24/13.
+//  Copyright (c) 2013 Prylis Inc. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
